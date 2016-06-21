a75efacd3c3407423ca3e2078a23b1db0fb92037
Alex <alex.lee3851@gmail.com>
2016-06-21 11:22:29 -0700

    linked database with text analyzer

--- src/server/controllers/apiController.js
+++ src/server/controllers/apiController.js
@@ -1,32 +1,49 @@
-import config from '../../../config';
-import fetch from 'isomorphic-fetch';
 import async from 'async';
+import VocabFetcher from '../helpers/wordFetcher';
+import redis from '../database/redis';
 
-const getWord = word => {
-  const key = config.mashape_key;
-  const options = {
-    headers: {
-      'X-Mashape-Key': key,
-      Accept: 'application/json',
-    },
-  };
-  const url = `https://wordsapiv1.p.mashape.com/words/${word}/entails`;
-  fetch(url, options)
-    .then(response => response.data)
-    .then();
+const dictionary = new VocabFetcher();
+
+const getDefinition = (word, cb) => {
+  dictionary.getWord(word)
+    .then(wordObj => wordObj.definitions)
+    .then(definitions => cb(definitions));
+};
+
+const analyzeString = (text, filterWords, callback) => {
+  const words = text.split(' ');
+  const filtered = words.filter(word => filterWords.indexOf(word) === -1);
+  const object = [];
+  let verb = null;
+
+  async.eachSeries(filtered, (word, cb) => {
+    getDefinition(word, definitions => {
+      for (let i = 0; i < definitions.length; i++) {
+        const partOfSpeech = definitions[i].partOfSpeech;
+        if (partOfSpeech === 'v' && !verb) {
+          verb = word;
+          i = definitions.length;
+        } else if (partOfSpeech === 'adj' || partOfSpeech === 'n') {
+          object.push(word);
+          i = definitions.length;
+        }
+      }
+      cb();
+    });
+  }, err => {
+    // eslint-disable-next-line no-console
+    console.log(err);
+    callback({ verb, object });
+  });
 };
 
 const analyze = (req, res) => {
-  const text = req.body.word;
-  const words = text.split();
-  const filtered = words.filter(word => {
-
+  const text = req.body.text || '';
+  redis.retrieveFilteredWords(filterWords => {
+    analyzeString(text, filterWords, data => {
+      res.send(data);
+    });
   });
-  for (let i = 0; i < filtered.length; i++) {
-    getWord(filtered, cb);
-  }
-
-  res.send('Something');
 };
 
-export default { analyze, getWord };
+export default { analyze, getDefinition };
